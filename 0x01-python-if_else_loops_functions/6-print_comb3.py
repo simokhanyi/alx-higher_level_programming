34c5d815ee1c9534f55b6338c9a15dbf3c6cec77
Simo Khanyi <simokhanyi@gmail.com>
2023-10-03 16:11:36 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-for first_digit in range(10):
+# Iterate through the first digit
+for first_digit in range(0, 10):
+    # Iterate through the second digit, starting from the next one
     for second_digit in range(first_digit + 1, 10):
-        print(format"{first_digit}{second_digit}", end=', ')
-        
-# Print a newline character to end the last line
-print()
+        # Check if it's the last combination
+        if first_digit == 8 and second_digit == 9:
+            print("{}{}".format(first_digit, second_digit))
+        else:
+            print("{}{}".format(first_digit, second_digit), end=", ")
+
