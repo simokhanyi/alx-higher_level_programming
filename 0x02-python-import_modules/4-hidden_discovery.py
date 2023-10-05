6196ad65e936251b921052e4cde78a8fc71f4046
Simo Khanyi <simokhanyi@gmail.com>
2023-10-05 08:31:11 -0700

    Import and modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-import py_compile
-import marshal
 
-# Compile the .pyc file (if not already compiled)
-py_compile.compile("hidden_4.py")
 
-# Load the compiled module
-with open("hidden_4.pyc", "rb") as file:
-    code = marshal.load(file)
-
-# Filter and print names that do not start with '__' in alphabetical order
-names = sorted(name for name in code.co_names if not name.startswith('__'))
-for name in names:
-    print(name)
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
