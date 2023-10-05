22200ce3d05a4a493be435bb07291a48a1386913
Simo Khanyi <simokhanyi@gmail.com>
2023-10-05 08:24:03 -0700

    Import and modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]  # Exclude the script name from the arguments
+    import sys
+    i = len(sys.argv) - 1
 
-    num_args = len(argv)
-
-    if num_args == 0:
-        print("Number of argument(s): 0.")
-        print(".")
-    else:
-        print("Number of argument(s):", num_args)
-        print(":")
-        for i, arg in enumerate(argv, start=1):
-            print("{}: {}".format(i, arg))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+import py_compile
 import marshal
+
+# Compile the .pyc file (if not already compiled)
+py_compile.compile("hidden_4.py")
 
 # Load the compiled module
 with open("hidden_4.pyc", "rb") as file:
     code = marshal.load(file)
 
-# Extract and print names that do not start with '__'
-names = [name for name in code.co_names if not name.startswith('__')]
-names.sort()  # Sort the names alphabetically
+# Filter and print names that do not start with '__' in alphabetical order
+names = sorted(name for name in code.co_names if not name.startswith('__'))
 for name in names:
     print(name)
