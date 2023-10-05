05cb4db0cd9fb1768bf865a646271fffd01e8bc6
Simo Khanyi <simokhanyi@gmail.com>
2023-10-05 05:09:28 -0700

    Python - import & modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+if __name__ == "__main__":
+
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
