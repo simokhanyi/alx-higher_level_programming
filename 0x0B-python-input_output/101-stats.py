64264a4a8aa99eef504a5c576c38d6f583fb577a
Simo Khanyi <simokhanyi@gmail.com>
2023-11-07 17:11:08 -0800

    Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,57 @@
 #!/usr/bin/python3
 import sys
+import signal
 
 
-def print_stats(total_size, status_codes):
+def print_stats(total_size, status_counts):
     """
-    Prints the statistics based on total file size and status codes.
+    Print statistics including total file size and status code counts.
 
     :param total_size: Total file size.
-    :type total_size: int
-    :param status_codes: A dictionary containing status codes and counts.
-    :type status_codes: dict
+    :param status_counts: A dictionary containing status code counts.
     """
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    print(f"File size: {total_size}")
+    for code, count in sorted(status_counts.items()):
+        if count > 0:
+            print(f"{code}: {count}")
 
 
-try:
-    total_size = 0
-    status_codes = {str(code): 0 for code in (200, 301, 400, 401, 403, 404, 405, 500)}
+def parse_log(line, total_size, status_counts):
+    """
+    Parse a log line and update the total size and status code counts.
 
+    :param line: The log line to parse.
+    :param total_size: Total file size.
+    :param status_counts: A dictionary containing status code counts.
+    :return: A tuple with the updated total size and status counts.
+    """
+    parts = line.split()
+    if len(parts) >= 9:
+        status_code = parts[-2]
+        file_size = int(parts[-1])
+        total_size += file_size
+        if status_code in status_counts:
+            status_counts[status_code] += 1
+    return total_size, status_counts
+
+
+def main():
+    total_size, status_counts = 0, {"200": 0, "301": 0, "400": 0, "401": 0,
+                                    "403": 0, "404": 0, "405": 0, "500": 0}
     line_number = 0
 
-    for line in sys.stdin:
-        line_number += 1
-        fields = line.split()
-        if len(fields) >= 2:
-            status_code = fields[-2]
-            file_size = int(fields[-1])
-            total_size += file_size
+    try:
+        for line in sys.stdin:
+            line_number += 1
+            total_size, status_counts = parse_log(line, total_size,
+                                                  status_counts)
 
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+            if line_number % 10 == 0:
+                print_stats(total_size, status_counts)
 
-        if line_number % 10 == 0:
-            print_stats(total_size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(total_size, status_counts)
 
-except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
+
+if __name__ == "__main__":
+    main()
