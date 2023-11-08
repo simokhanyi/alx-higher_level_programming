579fd20b0ad2610a258724c5f7147e200d612dbb
Simo Khanyi <simokhanyi@gmail.com>
2023-11-08 13:11:41 -0800

    Python - Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,56 +5,15 @@ Module with class BaseGeometry
 
 
 class BaseGeometry:
-    """
-    A class that represents a basic geometry object.
-    """
+    """ Class with basic geometry methods """
 
     def area(self):
-        """
-        Calculate the area of the geometry object.
-
-        Raises:
-            Exception: This method is not implemented.
-        """
+        """ Calculate the area (not implemented in BaseGeometry) """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validate the value as an integer.
-
-        Args:
-            name (str): The name of the value.
-            value: The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
+        """ Validate that a value is a positive integer """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-# Test the code
-
-
-if __name__ == '__main__':
-    bg = BaseGeometry()
-
-    bg.integer_validator("my_int", 12)
-    bg.integer_validator("width", 89)
-
-    try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+            raise ValueError(f"{name} must be greater than 0")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,70 +5,20 @@ Module with class Rectangle
 """
 
 
-class BaseGeometry:
-    """
-    A class that defines basic geometric operations.
-    """
-
-    def area(self):
-        """
-        Calculate the area of the geometric shape.
-
-        Raises:
-            Exception: This method is not implemented.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate if a value is an integer and greater than 0.
-
-        Args:
-            name (str): The name of the value.
-            value: The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
-    """
-    A class that represents a rectangle and inherits from BaseGeometry.
-    """
+    """ Class representing a rectangle """
 
     def __init__(self, width, height):
-        """
-        Initialize a Rectangle instance with width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ Constructor """
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def area(self):
-        """
-        Calculate and return the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
+        """ Calculate the area of the rectangle """
         return self.__width * self.__height
 
-
-if __name__ == '__main__':
-    r = Rectangle(3, 5)
-    print(r.area())
+    def __str__(self):
+        """ Return a string representation of the rectangle """
+        return f"[Rectangle] {self.__width}/{self.__height}"
