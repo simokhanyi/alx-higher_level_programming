ec984fe501222ee57542d846b9d0316c98e12559
Simo Khanyi <simokhanyi@gmail.com>
2023-11-17 01:39:28 -0800

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,68 +1,101 @@
 #!/usr/bin/python3
 """
-Module that tests Rectangle
+Module with Rectangle
 """
 from models.base import Base
 
 
-class TestRectangle(Base):
+class Rectangle(Base):
     """
-    Test cases for the Rectangle class in rectangle.py.
+    This class represents a rectangle.
+
+    Attributes:
+        width (int): Width of the rectangle.
+        height (int): Height of the rectangle.
+        x (int): X-coordinate of the rectangle.
+        y (int): Y-coordinate of the rectangle.
+        id (int): Identifier of the rectangle.
     """
 
-    def test_rectangle_instance_creation(self):
-        """Test the creation of a Rectangle instance and the initialization."""
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
-
-    def test_rectangle_instance_with_attributes(self):
-        """Test creating instance with specified attributes."""
-        r = Rectangle(5, 3, 2, 1, 50)
-        self.assertEqual(r.id, 50)
-        self.assertEqual(r.width, 5)
-        self.assertEqual(r.height, 3)
-        self.assertEqual(r.x, 2)
-        self.assertEqual(r.y, 1)
-
-    def test_rectangle_area(self):
-        """Test the area method of the Rectangle class."""
-        r = Rectangle(7, 4)
-        self.assertEqual(r.area(), 28)
-
-    def test_rectangle_str_representation(self):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Test the __str__ method for the string representation of Rectangle.
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int, optional): X-coordinate of the rectangle (default is 0).
+            y (int, optional): Y-coordinate of the rectangle (default is 0).
+            id (int, optional): Identifier of the rectangle (default is None).
         """
-        r = Rectangle(3, 8, 2, 1, 99)
-        self.assertEqual(str(r), "[Rectangle] (99) 2/1 - 3/8")
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-    def test_rectangle_update_method(self):
-        """Test the update method for updating attributes of Rectangle."""
-        r = Rectangle(4, 6, 1, 2, 10)
-        r.update(89)
-        self.assertEqual(str(r), "[Rectangle] (89) 1/2 - 4/6")
-        r.update(89, 2)
-        self.assertEqual(str(r), "[Rectangle] (89) 1/2 - 2/6")
-        r.update(89, 2, 3)
-        self.assertEqual(str(r), "[Rectangle] (89) 1/2 - 2/3")
-        r.update(89, 2, 3, 4)
-        self.assertEqual(str(r), "[Rectangle] (89) 4/2 - 2/3")
-        r.update(89, 2, 3, 4, 5)
-        self.assertEqual(str(r), "[Rectangle] (89) 4/5 - 2/3")
+    @staticmethod
+    def save_to_file_csv(list_objs):
+        # Implement the method to save objects to CSV file
+        pass
 
-    def test_rectangle_to_dictionary(self):
-        """Test the to_dictionary method for Rectangle."""
-        r = Rectangle(4, 6, 1, 2, 10)
-        r_dictionary = r.to_dictionary()
-        expected_dict = {
-            'id': 10,
-            'width': 4,
-            'height': 6,
-            'x': 1,
-            'y': 2
+    @classmethod
+    def load_from_file_csv(cls):
+        # Implement the method to load objects from CSV file
+        pass
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """Display the rectangle with '#' characters."""
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance.
+
+        Returns:
+            str: String representation of the Rectangle.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
+
+    def update(self, *args, **kwargs):
+        """Update the attributes of the Rectangle."""
+        attr_names = ['id', 'width', 'height', 'x', 'y']
+        if args:
+            for i, arg in enumerate(args):
+                setattr(self, attr_names[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of the Rectangle.
+
+        Returns:
+            dict: Dictionary containing id, width, height, x, and y.
+        """
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
         }
-        self.assertDictEqual(r_dictionary, expected_dict)
 
+    def to_csv(self):
+        """Returns rectangle attributes as a list for CSV."""
+        return [self.id, self.width, self.height, self.x, self.y]
 
-if __name__ == '__main__':
-    unittest.main()
+    @classmethod
+    def create_from_csv(cls, row):
+        """Creates a Rectangle instance from CSV row data."""
+        return cls(*map(int, row))
