55b75bbf59d55debfffa711a1b5c6cbc6e18e003
Simo Khanyi <simokhanyi@gmail.com>
2023-12-12 06:56:10 -0800

    SQL - Introduction

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script that computes the average score of all records in the table second_table in my MySQL server.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- Script that lists all tables of a database on my MySQL server.
+SHOW TABLES;
