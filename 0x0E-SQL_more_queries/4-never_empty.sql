07f7da9c86ec9c6ad9c994779bfd8583f395e425
Simo Khanyi <simokhanyi@gmail.com>
2023-12-13 12:58:39 -0800

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- Scripr that creates the table id_not_null on your MySQL server
+-- Script that creates a table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- Script that creates the table unique_id on your MySQL server
+-- Script that creates a table
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
