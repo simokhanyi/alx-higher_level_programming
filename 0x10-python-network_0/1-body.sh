fd5cb72e8a34c580f3fbc1f8f70fd26c2fc6df1c
Simo Khanyi <simokhanyi@gmail.com>
2024-01-25 13:08:39 -0800

    Python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a GET request to the URL, and displays the body 200 of the response
-[ $# -eq 1 ] && curl -s -o /dev/stdout -w "%{http_code}" "$1" | awk 'NR>1 {print}'
+[ $# -eq 1 ] && url=$1 && curl -s -o /dev/stdout -w "%{http_code}" "$url" | awk 'NR>1 {print}'
